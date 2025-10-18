cc4bee0a8349cc5e1804ccd8108e94f4d50fb412
rkroy <rajr86642@gmail.com>
2025-10-18 17:46:49 +0530

    Update : UI(Animations)

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -2,13 +2,12 @@
 import DefaultTheme from "vitepress/theme";
 import CodeTabs from "./components/CodeTabs.vue";
 import ElectricCard from "./components/ElectricCard.vue";
+import "./custom.css";
 
 export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.component("CodeTabs", CodeTabs);
-    app.component("ElectricCard", ElectricCard);
-
-    
-  },
+	...DefaultTheme,
+	enhanceApp({ app }) {
+		app.component("CodeTabs", CodeTabs);
+		app.component("ElectricCard", ElectricCard);
+	},
 };
