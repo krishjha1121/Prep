f0fd0818d1a1fd8544953ad817df35d76b7c4243
Rk Roy🇳🇵 <rajr86642@gmail.com>
2025-10-26 14:54:11 +0530

    Add : Leetcode Solutions

--- generateProblem.js
+++ generateProblem.js
@@ -1,31 +1,33 @@
-import fs from "fs";
-import fetch from "node-fetch";
+// This script generates problems.json(all the problems number with their difficulty) from leetcode.com API
+
+import fs from 'fs'
+import fetch from 'node-fetch'
 
 async function main() {
-	try {
-		const res = await fetch("https://leetcode.com/api/problems/all/");
-		const json = await res.json();
+    try {
+        const res = await fetch('https://leetcode.com/api/problems/all/')
+        const json = await res.json()
 
-		const problems = {};
+        const problems = {}
 
-		json.stat_status_pairs.forEach((p) => {
-			const id = p.stat.question_id;
-			const slug = p.stat.question__title_slug;
-			const difficulty =
-				["easy", "medium", "hard"][p.difficulty.level - 1] || "medium";
-			problems[id] = { slug, difficulty };
-		});
+        json.stat_status_pairs.forEach((p) => {
+            const id = p.stat.question_id
+            const slug = p.stat.question__title_slug
+            const difficulty =
+                ['easy', 'medium', 'hard'][p.difficulty.level - 1] || 'medium'
+            problems[id] = { slug, difficulty }
+        })
 
-		if (!fs.existsSync("./public/data")) fs.mkdirSync("./public/data");
+        if (!fs.existsSync('./public/data')) fs.mkdirSync('./public/data')
 
-		fs.writeFileSync(
-			"./public/data/problems.json",
-			JSON.stringify(problems, null, 2),
-		);
-		console.log("✅ problems.json generated successfully!");
-	} catch (err) {
-		console.error("❌ Failed to generate problems.json:", err);
-	}
+        fs.writeFileSync(
+            './public/data/problems.json',
+            JSON.stringify(problems, null, 2),
+        )
+        console.log('✅ problems.json generated successfully!')
+    } catch (err) {
+        console.error('❌ Failed to generate problems.json:', err)
+    }
 }
 
-main();
+main()
